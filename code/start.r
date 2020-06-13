49a0de8cc88c1ee314e5692d69c7361a193ff9f7
Dietz Vollrath <dietz.vollrath@gmail.com>
2020-06-13 09:05:55 -0500

    Update figures

--- code/start.r
+++ code/start.r
@@ -8,9 +8,19 @@ Sys.setenv("plotly_api_key"="g86hGs9hiBrHVnbWlYGr")
 data("pwt9.1")
 
 p <- pwt9.1
-p$lngdppc <- log(p$rgdpe) - log(p$pop)
+p$lngdppc <- log(p$rgdpna) - log(p$pop)
 
-newp <- p[ which(p$isocode %in% c("USA", "CAN")),]
+stable <- p[ which(p$isocode %in% c("USA", "CAN", "MEX", "GBR", "AUS")),]
+catchup <- p[ which(p$isocode %in% c("USA", "DEU", "JPN", "KOR", "CHN","NGA")),]
 
-fig <- plot_ly(newp, x = ~year, y = ~lngdppc, linetype = ~isocode, type = 'scatter', mode = 'lines+markers')
-api_create(fig, filename = "pwt-usa-lngdppc")
+fig <- plot_ly(stable, x = ~year, y = ~lngdppc, linetype = ~country, type = 'scatter', mode = 'lines+markers')
+fig <- layout(fig, title = 'Log GDP per capita for stable growth countries',
+               xaxis = list(title = 'Year'),
+               yaxis = list (title = 'Log GDP per capita', range=c(6,11)))
+api_create(fig, filename = "pwt-stable-lngdppc")
+
+fig <- plot_ly(catchup, x = ~year, y = ~lngdppc, linetype = ~country, type = 'scatter', mode = 'lines+markers')
+fig <- layout(fig, title = 'Log GDP per capita for catch-up growth countries',
+              xaxis = list(title = 'Year'),
+              yaxis = list (title = 'Log GDP per capita', range=c(6,11)))
+api_create(fig, filename = "pwt-catchup-lngdppc")
