875de116c720f0db8eebf51d3c01f6ec2d1a22dc
Dietz Vollrath <dietz.vollrath@gmail.com>
2020-06-13 08:10:06 -0500

    Try own PWT plot

--- code/start.r
+++ code/start.r
@@ -1,8 +1,16 @@
 library(devtools)
 library(plotly)
+library(pwt9)
 
 Sys.setenv("plotly_username"="dvollrath")
 Sys.setenv("plotly_api_key"="g86hGs9hiBrHVnbWlYGr")
 
-fig <- plot_ly(midwest, x = ~percollege, color = ~state, type = "box")
-api_create(fig, filename = "r-docs-midwest-boxplots")
+data("pwt9.1")
+
+p <- pwt9.1
+p$lngdppc <- log(p$rgdpe) - log(p$pop)
+
+newp <- p[ which(p$isocode %in% c("USA", "CAN")),]
+
+fig <- plot_ly(newp, x = ~year, y = ~lngdppc, linetype = ~isocode, type = 'scatter', mode = 'lines+markers')
+api_create(fig, filename = "pwt-usa-lngdppc")
--- introduction.md
+++ introduction.md
@@ -6,7 +6,7 @@ nav_order: 1
 ## Testing Plotly
 Let's give this a shot
 
-<iframe width="900" height="800" frameborder="0" scrolling="no" src="//plotly.com/~dvollrath/1.embed"></iframe>
+<iframe width="600" height="400" frameborder="0" scrolling="no" src="//plotly.com/~dvollrath/5.embed"></iframe>
 
 ## Comparing levels and growth rates
 Rich versus poor, but growing at similar rates?
