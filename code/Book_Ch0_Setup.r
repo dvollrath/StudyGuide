87c4dcb81493d44c422f3b5da7a0793b9faf1a71
dvollrath <dietz.vollrath@gmail.com>
2022-08-23 13:28:14 -0500

    Clean up some code

--- code/Book_Ch0_Setup.r
+++
@@ -1,9 +0,0 @@
-setwd("~/Dropbox/project/studyguide/data")
-drafts = "~/Dropbox/project/Growth4ed/Figures" # folder for static images
-source("~/Dropbox/Tools/.Rprofile")
-library(pwt10)
-library(ggplot2)
-library(dplyr)
-library(scales)
-library(tidyr)
-library(plotly)
