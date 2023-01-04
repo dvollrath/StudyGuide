acccf42421990da4fb213f96ade41a6f64e304fa
dvollrath <dietz.vollrath@gmail.com>
2023-01-04 12:08:03 -0600

    Chapter 5 answers

--- code/ProdBasic/app.R
+++ code/ProdBasic/app.R
@@ -5,13 +5,14 @@ library(ggplot2)
 #############################################################
 # Set the baseline parameters
 #############################################################
-base_lambda <- .8
+base_lambda <- 1
 base_phi <- 0
-base_R_0 <- 1
-base_A_0 <- 2
+base_L_0 <- 1
+base_sR <-.02
 base_theta <- .1
-base_gR <- .02
-base_timeT <- 50
+base_gL <- .01
+base_timeT <- 200
+base_A_0 <- ((base_L_0^base_lambda)*(1-base_phi)*base_theta*(base_sR^base_lambda)/(base_lambda*base_gL))^(1/(1-base_phi))
 
 #############################################################
 # Define UI ----
@@ -25,16 +26,20 @@ ui <- fluidPage(
       
       h4("Set parameters"),
       actionButton("reset", "Reset to baseline"),
-      sliderInput("alt_R", h6('Initial researchers \\(R_0\\)'),
-                  min = 0, max = 10, value = base_R_0, step=.01),
+      sliderInput("alt_L", h6('Initial population \\(L_0\\)'),
+                  min = 0, max = 4, value = base_L_0, step=.01),
+      sliderInput("alt_A", h6('Initial productivity \\(A_0\\)'),
+                  min = 0, max = 4, value = base_A_0, step=.01),
+      sliderInput("alt_sR", h6('Initial research share \\(s_R\\)'),
+                  min = 0, max = .5, value = base_sR, step=.01),
       sliderInput("alt_lambda", h6("Research elasticity \\(\\lambda\\)"),
                   min = 0, max = 1, value = base_lambda),
       sliderInput("alt_phi", h6("Productivity effect \\(\\phi\\)"),
                   min = -2, max = 1, value = base_phi,step=.01),
       sliderInput("alt_theta", h6("Base parameter \\(\\theta\\)"),
                   min = 0, max = 1, value = base_theta,step=.01),
-      sliderInput("alt_gR", h6("Researcher growth \\(g_R\\)"),
-                  min = 0, max = .2, value = base_gR,step=.005),      
+      sliderInput("alt_gL", h6("Population growth \\(g_L\\)"),
+                  min = 0, max = .05, value = base_gL,step=.005),      
       sliderInput("timeT", h6("Number of periods"),
                   min = 0, max = 300, value = base_timeT, step = 1)
       ), # end sidebar panel
@@ -58,9 +63,9 @@ ui <- fluidPage(
                            plotOutput("lnagraph",width = "100%") ),
                   tabPanel("Productivity Dynamics",
                            br(),
-                           p("This figure describes how the ratio of \\(R^{\\lambda}/A^{1-\\phi}\\) determines
+                           p("This figure describes how the ratio of \\(L^{\\lambda}/A^{1-\\phi}\\) determines
                              the growth rate of productivity, which is the upward sloping line. The horizontal 
-                             lines are the scaled researcher growth rate, \\(g_R\\lambda/(1-\\phi)\\). The
+                             lines are the scaled researcher growth rate, \\(g_L\\lambda/(1-\\phi)\\). The
                              dynamics dictate that eventually the economy ends up at where those two lines cross.
                              The baseline dynamics are in blue, the alternative in green, and the black dots
                              indicate the time path that productivity growth actually take. The red lines indicate
@@ -79,13 +84,14 @@ server <- function(input, output, session) {
 
   # Reset parameters
   observeEvent(input$reset, {
-    updateSliderInput(session,'alt_R',value = base_R_0)
+    updateSliderInput(session,'alt_L',value = base_L_0)
     updateSliderInput(session,'alt_A',value = base_A_0)
     updateSliderInput(session,'alt_lambda',value = base_lambda)
     updateSliderInput(session,'alt_phi',value = base_phi)
     updateSliderInput(session,'alt_theta',value = base_theta)
     updateSliderInput(session,'timeT',value = base_timeT)
-    updateSliderInput(session,'alt_gR',value = base_gR)
+    updateSliderInput(session,'alt_gL',value = base_gL)
+    updateSliderInput(session,'alt_sR',value = base_sR)
   })
   
 
@@ -94,41 +100,38 @@ server <- function(input, output, session) {
   GraphData <- reactive({
     t <- c(0:input$timeT) # vector of periods up to input time
     df <- data.frame(t) # create dataframe to hold outcomes
-    lnR <- log(input$alt_R)
     
-    gAbase <- base_gR*base_lambda/(1-base_phi) # base s.s. growth rate
-    Astarbase <- (base_theta*base_R_0^(base_lambda)/(gAbase))^(1/(1-base_phi)) # base init A
-    lnA <- log(Astarbase) # initialize actual A series
-    lnAstarbase <- log(Astarbase) # log for graphing
-    gAalt <- input$alt_gR*input$alt_lambda/(1-input$alt_phi) # alt s.s. growth rate
-    Astaralt <- (input$alt_theta*input$alt_R^(input$alt_lambda)/(gAalt))^(1/(1-input$alt_phi))
-    lnAstaralt <- log(Astaralt)
+    AL_base_init <- base_A_0^(1-base_phi)/base_L_0^base_lambda # initial A/L ratio (with exponents)
+    AL_alt_init <- input$alt_A^(1-input$alt_phi)/input$alt_L^input$alt_lambda # initial A/L ratio (with exponents)
+    AL_base_ss <- (1-base_phi)*base_theta*(base_sR^base_lambda)/(base_lambda*base_gL) # s.s. A/L ratio
+    AL_alt_ss <- (1-input$alt_phi)*input$alt_theta*(input$alt_sR^input$alt_lambda)/(input$alt_lambda*input$alt_gL) # s.s. A/L ratio
+    gA_base_ss <- (base_lambda/(1-base_phi))*base_gL # steady state growth rate of A
+    gA_alt_ss <- (input$alt_lambda/(1-input$alt_phi))*input$alt_gL # steady state growth rate of A
     
-    for (i in t) {
-      df$ga[i+1] <- input$alt_theta*exp(lnR)^(input$alt_lambda)/exp(lnA)^(1-input$alt_phi)
-      df$lnA[i+1] <- lnA
-      df$lnR[i+1] <- lnR
-      lnR <- lnR+input$alt_gR
-      lnA <- lnA+df$ga[i+1]
-    }
-    df$gabase <- base_gR*base_lambda/(1-base_phi)
-    df$gaalt <- input$alt_gR*input$alt_lambda/(1-input$alt_phi)
-    df$lnAbase <- lnAstarbase + df$gabase*df$t
-    df$lnAalt <- lnAstaralt + df$gaalt*df$t
+    alt_conv_term <- input$alt_lambda*input$alt_gL # convergence term for exact solution
+    df$AL_alt <- AL_alt_ss*(1-exp(-1*alt_conv_term*df$t)) + AL_alt_init*exp(-1*alt_conv_term*df$t) # A/L at each t
+    df$gA_alt_time <- input$alt_theta*(input$alt_sR^input$alt_lambda)/df$AL_alt # gA at each t
+    df$lnA <- (input$alt_lambda/(1-input$alt_phi))*log(input$alt_L) + (1/(1-input$alt_phi))*log(df$AL_alt) + (input$alt_lambda/(1-input$alt_phi))*input$alt_gL*df$t
+    df$lnA_alt_bgp <- (input$alt_lambda/(1-input$alt_phi))*log(input$alt_L) + (1/(1-input$alt_phi))*log(AL_alt_ss) + (input$alt_lambda/(1-input$alt_phi))*input$alt_gL*df$t
+    df$lnA_base_bgp <- (base_lambda/(1-base_phi))*log(base_L_0) + (1/(1-base_phi))*log(AL_base_ss) + (base_lambda/(1-base_phi))*base_gL*df$t
     
-    df$tick <- df$ga/input$alt_theta
+    df$tick <- df$gA_alt_time/(input$alt_theta*input$alt_sR^input$alt_lambda)
+    df$gA_base_ss <- gA_base_ss
+    df$gA_alt_ss <- gA_alt_ss
     return(df)
   })
   
   output$gagraph <- renderPlot(
     {
       ggplot(GraphData(), aes(t,value,colour=variable)) +
-        geom_line(aes(y = ga, color = "black"),size=1) + 
-        geom_line(aes(y = gabase, color="blue"), linetype="dashed",size=1) +
-        geom_line(aes(y = gaalt, color="green"), linetype="dashed",size=1) +
+        geom_line(aes(y = gA_alt_time, color = "black"),size=1) + 
+        geom_line(aes(y = gA_base_ss, color="blue"), linetype="dashed",size=1) +
+        geom_line(aes(y = gA_alt_ss, color="green"), linetype="dashed",size=1) +
         xlab("Time") +
         ylab("Growth rate of productivity") +
         theme_light() +
+        theme(text = element_text(size = 16)) +
+        ylim(0, .04) +
         ggtitle("Produtcivity growth over time") + 
         scale_color_identity(name = "Models",
                              breaks = c("black", "green", "blue"),
@@ -141,12 +144,13 @@ server <- function(input, output, session) {
     {
       ggplot(GraphData(), aes(t,value,colour=variable)) +
         geom_line(aes(y = lnA, color = "black"),size=1) + 
-        geom_line(aes(y = lnAbase, color="blue"), linetype="dashed",size=1) +
-        geom_line(aes(y = lnAalt, color="green"), linetype="dashed",size=1) +
+        geom_line(aes(y = lnA_base_bgp, color="blue"), linetype="dashed",size=1) +
+        geom_line(aes(y = lnA_alt_bgp, color="green"), linetype="dashed",size=1) +
         xlab("Time") +
         ylab("Log of productivity") +
         #ylim(0, .1) +
         theme_light() +
+        theme(text = element_text(size = 16)) +
         ggtitle("Produtcivity level over time") + 
         scale_color_identity(name = "Models",
                              breaks = c("black", "green", "blue"),
@@ -157,31 +161,32 @@ server <- function(input, output, session) {
   
   output$dyngraph <- renderPlot(
     {
-      tick <- seq(0,1,.01) # arbitrary ky values
-      df <- data.frame(tick) # create dataframe
-      gRbase <- base_gR*base_lambda/(1-base_phi)
-      gRalt <- input$alt_gR*input$alt_lambda/(1-input$alt_phi)
-      df$gAbase <- base_theta*df$tick
-      df$gAalt <- input$alt_theta*df$tick
-      ssbase <- (base_gR*base_lambda/(1-base_phi))/base_theta
-      ssalt <- (input$alt_gR*input$alt_lambda/(1-input$alt_phi))/input$alt_theta
+      tick <- seq(0,10,.01) # arbitrary ky values
+      dm <- data.frame(tick) # create dataframe
+      gAbasess <- base_gL*base_lambda/(1-base_phi)
+      gAaltss <- input$alt_gL*input$alt_lambda/(1-input$alt_phi)
+      dm$gAbase <- base_theta*base_sR^base_lambda*dm$tick
+      dm$gAalt <- input$alt_theta*input$alt_sR^input$alt_lambda*dm$tick
+      ssbase <- (base_gL*base_lambda/(1-base_phi))/(base_theta*base_sR^base_lambda)
+      ssalt <- (input$alt_gL*input$alt_lambda/(1-input$alt_phi))/(input$alt_theta*input$alt_sR^input$alt_lambda)
       
-      ggplot(df, aes(x=tick)) +
+      ggplot(dm, aes(x=tick)) +
         geom_line(aes(y = gAalt, color = "green"),size=1) + 
         geom_line(aes(y = gAbase, color="blue"), linetype="dashed",size=1) +
-        geom_point(data=GraphData(),y=GraphData()$ga, color="black") +
-        geom_hline(yintercept=gRbase, linetype="dashed", color = "blue") +
-        geom_hline(yintercept=gRalt, linetype="dashed", color = "green") +
+        geom_point(data=GraphData(),y=GraphData()$gA_alt_time, color="black") +
+        geom_hline(yintercept=gAbasess, linetype="dashed", color = "blue") +
+        geom_hline(yintercept=gAaltss, linetype="dashed", color = "green") +
         geom_vline(xintercept=ssbase, linetype="dotted", color = "red") +
         geom_vline(xintercept=ssalt, linetype="dotted", color = "red") +
-        xlab("Relative R/A (R^l/A^1-p)") +
+        xlab("Relative L/A (L^l/A^1-p)") +
         ylab("Growth rates") +
         theme_light() +
-        ylim(0,.10) +
+        theme(text = element_text(size = 16)) +
+        ylim(0,.05) +
         ggtitle("Productivity growth dynamics") + 
         scale_color_identity(name = "Models",
                              breaks = c("green", "blue"),
-                             labels = c("New BGP", "Old BGP"),
+                             labels = c("New gA function", "Old gA function"),
                              guide = "legend")
     }
   ) # end dyn graph  
