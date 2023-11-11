2478ed8aa27ad6ac0f987c81db4a77f2f1d682a8
dvollrath <dietz.vollrath@gmail.com>
2023-11-11 12:42:23 -0600

    Add tables

--- code/Guide_PWT_DevAccount.r
+++ code/Guide_PWT_DevAccount.r
@@ -17,6 +17,14 @@ p$tfp <- p$gdppc/(p$ky*p$hc) # just the TFP part
 
 p <- p[which(p$year == "2019"),] # just the 2019 data
 
+s <- p[,c("country","rgdpna","pop","rnna","hc","emp")] # raw data
+s$rgdpna <- round(s$rgdpna,digits=2)
+s$pop <- round(s$pop,digits=2)
+s$rnna <- round(s$rnna,digits=2)
+s$hc <- round(s$hc,digits=2)
+s$emp <- round(s$emp,digits=2)
+names(s) <- c("Country","GDP","Pop.(N)","K","HC p.c.","Worker(L)")
+
 usgdppc <- with(p,gdppc[isocode=="USA"])
 usky <- with(p,ky[isocode=="USA"])
 ushc <- with(p,hc[isocode=="USA"])
@@ -37,9 +45,9 @@ p$acctlfp <- round(p$acctlfp,digits=3)
 p$accttfp <- round(p$accttfp,digits=3)
 names(p) <- c("Country","GDP p.c.","K/Y","HC","L/N","A")
 
-p <- p[complete.cases(p), ]
 
-s <- p[which(p$isocode %in% c("USA","FRA","CHL","POL","AUS","THA")),]
+
+p <- p[complete.cases(p), ]
 
 #########################################################################
 # Figure for level of R&D workers and level of productivity
@@ -51,7 +59,16 @@ tab <- datatable(p,rownames = FALSE,  class = 'cell-border stripe',
                  caption = 'Development accounting for 2019',
                  extensions = 'Buttons', 
                  options=list(dom = 'lfrtipB',
-                              buttons = c('copy', 'csv')
+                              buttons = c('csv')
                               )
                  )
 saveWidget(tab, "../plotly/pwt-dev-acct.html",selfcontained = F, libdir = "lib")
+
+tab <- datatable(s,rownames = FALSE,  class = 'cell-border stripe', 
+                 caption = 'Raw data for 2019',
+                 extensions = 'Buttons', 
+                 options=list(dom = 'lfrtipB',
+                              buttons = c('csv')
+                 )
+)
+saveWidget(tab, "../plotly/pwt-dev-raw.html",selfcontained = F, libdir = "lib")
--- develop/accounting.md
+++ develop/accounting.md
@@ -11,5 +11,13 @@ nav_order: 1
 1. TOC 
 {:toc}
 
-## Blah
+## Comparisons countries
+
+
 <iframe width="900" height="600" frameborder="0" scrolling="no" src="../plotly/pwt-dev-acct.html"></iframe>
+
+
+## Raw data
+The raw data necessary to make all the development accounting calculations is in the following table. The human capital per capita term is given directly by the Penn World Tables. 
+
+<iframe width="900" height="600" frameborder="0" scrolling="no" src="../plotly/pwt-dev-raw.html"></iframe>
