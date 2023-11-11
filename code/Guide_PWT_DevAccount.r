61cb81a3848d896eb16118fcb3818652109011f2
dvollrath <dietz.vollrath@gmail.com>
2023-11-11 11:52:22 -0600

    Add DT

--- code/Guide_PWT_DevAccount.r
+++ code/Guide_PWT_DevAccount.r
@@ -1,6 +1,6 @@
 # Do development accounting and prepare table of results
 
-
+library(DT)
 #########################################################################
 # Pull PWT into dataframe
 data("pwt10.01") # extract PWT data
@@ -29,14 +29,18 @@ p$accthc <- p$hc/ushc
 p$acctlfp <- p$lfp/uslfp
 p$accttfp <- p$tfp/ustfp
 
+p <- p[,c("country","acctgdppc","acctky","accthc","acctlfp","accttfp")]
+p$acctgdppc <- round(p$acctgdppc,digits=3)
+p$acctky <- round(p$acctky,digits=3)
+p$accthc <- round(p$accthc,digits=3)
+p$acctlfp <- round(p$acctlfp,digits=3)
+p$accttfp <- round(p$accttfp,digits=3)
+names(p) <- c("Country","GDP p.c.","K/Y","HC","L/N","A")
+
+p <- p[complete.cases(p), ]
+
 s <- p[which(p$isocode %in% c("USA","FRA","CHL","POL","AUS","THA")),]
-s <- s[,c("country","acctgdppc","acctky","accthc","acctlfp","accttfp")]
-s$acctgdppc <- round(s$acctgdppc,digits=3)
-s$acctky <- round(s$acctky,digits=3)
-s$accthc <- round(s$accthc,digits=3)
-s$acctlfp <- round(s$acctlfp,digits=3)
-s$accttfp <- round(s$accttfp,digits=3)
-names(s) <- c("Country","GDP p.c.","K/Y","HC","L/N","A")
+
 #########################################################################
 # Figure for level of R&D workers and level of productivity
 #########################################################################
@@ -63,4 +67,21 @@ fig <- plot_ly(
 
 saveWidget(fig, "../plotly/pwt-dev-acct.html",selfcontained = F, libdir = "lib")
 
-#api_create(fig, filename = "pwt-oecd-level-rd")
+datatable(p,rownames = FALSE,  class = 'cell-border stripe', options=list(buttons = c('excel')))
+
+dev_search <- DT::datatable((p),
+                               rownames = FALSE, 
+                               extensions = 'Buttons', 
+                               options = list(autoWidth = TRUE, 
+                                              extensions = 'FixedColumns', 
+                                              options = list(dom = 't', 
+                                                             scrollX = TRUE, 
+                                                             fixedColumns = TRUE),
+                                              columnDefs = list(list(width= '200px',
+                                                                     targets = "feedback")), 
+                                              dom=('Bfrtip'), 
+                                              buttons = c('excel'),
+                                              #pageLength = table_rows,
+                                              searchHighlight = TRUE),
+                               filter=list(position="top"))
+
